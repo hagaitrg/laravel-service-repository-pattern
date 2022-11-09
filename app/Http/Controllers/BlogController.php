30e0606a78201d828a554bb8ca007b24ef9e0270
hagaitrg <disahagait@gmail.com>
2022-11-09 21:45:09 +0700

    membuat controller

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repository\BlogRepository;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    private $blog;
+    public function __construct(BlogRepository $blog)
+    {
+        $this->blog = $blog;
+    } 
+
+    public function index()
+    {
+        return response()->json([
+            "success" => true,
+            "code" => 200,
+            "message" => "Berhasil get data blog!",
+            "data" => $this->blog->getAllBlogs()
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $blogData = $request->all();
+
+        return response()->json([
+            "success" => true,
+            "code" => 201,
+            "message" => "Berhasil membuat blog baru!",
+            "data" => $this->blog->createBlog($blogData)
+        ], 201);
+    }
+
+    public function show($blogId)
+    {
+        return response()->json([
+            "success" => true,
+            "code" => 200,
+            "message" => "Berhasil mendapatkan detail blog!",
+            "data" => $this->blog->getDetailBlog($blogId)
+        ], 200);
+    }
+
+    public function update(Request $request, $blogId)
+    {
+        $blogData = $request->all();
+
+        return response()->json([
+            "success" => true,
+            "code" => 200,
+            "message" => "Berhasil update data blog!",
+            "data" => $this->blog->updateBlog($blogId, $blogData)
+        ], 200);
+    }
+
+    public function destroy($blogId)
+    {
+        return response()->json([
+            "success" => true,
+            "code" => 200,
+            "message" => "Berhasil delete data blog!",
+            "data" => $this->blog->deleteBlog($blogId)
+        ], 200);
+        
+    }
+
+    public function getReadBlog()
+    {
+        return response()->json([
+            "success" => true,
+            "code" => 200,
+            "message" => "Berhasil mendapatkan data blog yang sudah dibaca!",
+            "data" => $this->blog->getReadBlog()
+        ], 200);
+    }
+}
