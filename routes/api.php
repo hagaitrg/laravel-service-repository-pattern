de0d7c09271b776a0c796f0452286da0eadcb9a4
hagaitrg <disahagait@gmail.com>
2022-11-09 21:55:07 +0700

    menambahkan api routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('blogs')->group(function(){
+    Route::get('/', [BlogController::class, 'index']);
+    Route::get('{blogId}', [BlogController::class,'show']);
+    Route::get('readonly', [BlogController::class, 'getReadBlog']);
+    Route::post('/', [BlogController::class,'store']);
+    Route::put('{blogId}', [BlogController::class, 'update']);
+    Route::delete('{blogId}', [BlogController::class, 'destroy']);
 });
